3e5fab5e2375fd9167fc9a294ca206ebebf7c753
rafau <adas.rafalowski@gmail.com>
2022-12-17 22:52:19 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use std::fs;
+use std::io;
+
+fn main() {
+    let mut data = vec![0;1];
+    let mut pointer = 0;
+    let mut pointermax = 0;
+    let mut iterations = 0;
+    println!("Working...");
+    let file = fs::read_to_string("input.txt")
+        .expect("input file not found");
+    let mut counter = 0;
+    let mut programcounter = 0;
+    while true{
+            if file.chars().nth(counter) == None{
+                break;
+            }
+
+            let mut character = file.chars().nth(counter).unwrap();
+            if character == '>'{
+                pointer+=1;
+            }
+            else if character == '<'{
+                pointer-=1;
+            }
+            if pointer > pointermax{
+                pointermax = pointer;
+                data.push(0);
+            }
+            if character == '+'{
+                data[pointer] += 1;
+            }
+            else if character == '-'{
+                data[pointer] -= 1;
+            }
+            if character == '.'{
+                print!("{}", char::from(data[pointer]));
+            }
+            if iterations == 0{
+                programcounter +=1;
+            }
+            if character == '['{
+                iterations = data[pointer]
+            }
+            else if character == ']'{
+                iterations-=1;
+                if iterations != 0{
+                    counter = programcounter;        
+                }
+            }
+            counter+=1;
+            
+
+    }
+}
