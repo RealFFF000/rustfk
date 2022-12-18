eebb4118a0efeeecab74ae50a6abd75ff7af9df9
rafau <adas.rafalowski@gmail.com>
2022-12-18 14:35:21 +0100

    not-working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::fs;
-fn main() {
-    let mut data = vec![0;1];
-    let mut pointer = 0;
-    let mut pointermax = 0;
+pub fn petla(x:usize,y:usize){
+    let mut pointer = x;
+    let mut pointermax = x;
     let mut iterations = 0;
-    println!("Working...");
-    let file = fs::read_to_string("input.txt")
-        .expect("input file not found");
-    let mut counter = 0;
+    let mut data = vec![0;1];
+    let mut counter = y;
     let mut programcounter = 0;
+    let file = fs::read_to_string("../input.txt")
+    .expect("input file not found");
     loop{
             if file.chars().nth(counter) == None{
                 break;
@@ -34,19 +33,26 @@ fn main() {
                 print!("{}", char::from(data[pointer]));
             }
             if iterations == 0{
-                programcounter +=1;
+                programcounter+=1;
             }
             if character == '['{
-                iterations = data[pointer]
+                petla(pointer,programcounter);
             }
             else if character == ']'{
                 iterations-=1;
-                if iterations != 0{
-                    counter = programcounter;        
+                if iterations == 0{
+                    break;
+                }else{
+                    counter = programcounter;
                 }
             }
             counter+=1;
             
 
     }
+}
+
+
+pub fn main(){
+    petla(0,0);
 }
